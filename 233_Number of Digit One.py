22b6a71a0bbf28662207d9fa8df3fa9afda838ca
YoEugene <dreammacer.yo@gmail.com>
2016-02-24 13:39:43 +0800

    format file name

--- 233_Number of Digit One.py
+++ 233_Number of Digit One.py
@@ -1,47 +1,46 @@
 class Solution(object):
-	def __init__(self):
-		self.table = [1]
 
-	# def countDigitOneSlow(self, n):
-	# 	counter = 0
-	# 	for i in range(1, n+1):
-	# 		numArray = str(i)
-	# 		for j in range(len(numArray)):
-	# 			if numArray[j] is '1':
-	# 				counter += 1
-	# 	return counter
+    def __init__(self):
+        self.table = [1]
 
+    # def countDigitOneSlow(self, n):
+    # 	counter = 0
+    # 	for i in range(1, n+1):
+    # 		numArray = str(i)
+    # 		for j in range(len(numArray)):
+    # 			if numArray[j] is '1':
+    # 				counter += 1
+    # 	return counter
 
-	def createTable(self, n):
-		for i in range(1, n+1):
-			self.table.append(10 * self.table[i-1] + 10**i)
+    def createTable(self, n):
+        for i in range(1, n + 1):
+            self.table.append(10 * self.table[i - 1] + 10**i)
 
+    def countDigitOne(self, n):
+        if n < 0:
+            return 0
 
-	def countDigitOne(self, n):
-		if n < 0:
-			return 0
+        digitOfNumber = len(str(n))
+        self.createTable(digitOfNumber - 2)
 
-		digitOfNumber = len(str(n))
-		self.createTable(digitOfNumber-2)
+        return self.recursiveCountDigitOne(n, digitOfNumber)
 
-		return self.recursiveCountDigitOne(n, digitOfNumber)
+    def recursiveCountDigitOne(self, n, digitOfNumber):
+        firstDigit = int(str(n)[0]) if str(n) != '' else 0
 
-	def recursiveCountDigitOne(self, n, digitOfNumber):
-		firstDigit = int(str(n)[0]) if str(n) != '' else 0
+        if digitOfNumber is 1:
+            if n is 0:
+                return 0
+            else:
+                return 1
 
-		if digitOfNumber is 1:
-			if n is 0:
-				return 0
-			else:
-				return 1
+        restDigit = int(str(n)[1:]) if str(n)[1:] != '' else 0
 
-		restDigit = int(str(n)[1:]) if str(n)[1:] != '' else 0
+        digitOfRestDigit = len(str(restDigit))
 
-		digitOfRestDigit = len(str(restDigit))
+        if firstDigit is 0:
+            return self.recursiveCountDigitOne(restDigit, digitOfRestDigit)
+        if firstDigit is 1:
+            return self.table[digitOfNumber - 2] + 1 + restDigit + self.recursiveCountDigitOne(restDigit, digitOfRestDigit)
 
-		if firstDigit is 0:
-			return self.recursiveCountDigitOne(restDigit, digitOfRestDigit)
-		if firstDigit is 1:
-			return self.table[digitOfNumber-2] + 1 + restDigit + self.recursiveCountDigitOne(restDigit, digitOfRestDigit)
-
-		return firstDigit * self.table[digitOfNumber-2] + 10 ** (digitOfNumber - 1) + self.recursiveCountDigitOne(restDigit, digitOfRestDigit)
+        return firstDigit * self.table[digitOfNumber - 2] + 10 ** (digitOfNumber - 1) + self.recursiveCountDigitOne(restDigit, digitOfRestDigit)
--- 322_Coin Change.py
+++ 322_Coin Change.py
@@ -1,32 +1,34 @@
 class Solution(object):
     # BFS + Branch and bound
+
     def __init__(self):
-        self.level_queue=[]
-        self.next_level_queue=[]
+        self.level_queue = []
+        self.next_level_queue = []
         # record whether is traveled or not
-        self.dic={}
+        self.dic = {}
+
     def coinChange(self, coins, amount):
         """
         :type coins: List[int]
         :type amount: int
         :rtype: int
         """
-        
+
         if not coins:
             return -1
         if amount == 0:
             return 0
-        
+
         coin_numbers = 1
         self.level_queue.append(amount)
-        
-        while self.level_queue != []: 
+
+        while self.level_queue != []:
             for new_amount in self.level_queue:
                 for number in coins:
-                    if new_amount - number == 0 :
+                    if new_amount - number == 0:
                         return coin_numbers
                     elif new_amount - number in self.dic:
-                        pass 
+                        pass
                     elif new_amount - number > 0:
                         self.next_level_queue.append(new_amount - number)
                         self.dic[new_amount - number] = coin_numbers
